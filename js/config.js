59e505371e250609686bc67dbd98332a49e2b59e
M-Abzakh <abzakh.mohammad@gmail.com>
2022-01-28 03:31:18 +0200

    updated userpool and client

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'sa-east-1_wzkpdlOcg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4j3hf9ouu2thgs0jlk7h35tdbl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'sa-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_qW8AjnK0w', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1gpf9tqfbo1klpiap1d6jph2om', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://an2eirbqsk.execute-api.sa-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://an2eirbqsk.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
